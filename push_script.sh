2424f4dcb068c2455aff4542273244faa86646d4
Skyfz <mikesap34@gmail.com>
2024-04-05 00:18:01 +0200

    Commit on 2024-04-05 00:18:01

--- push_script.sh
+++ push_script.sh
@@ -11,3 +11,6 @@ git commit -m "Commit on $current_date_time"
 
 # Push changes to the 'main' branch on the remote named 'origin'
 git push origin main
+
+# Add a read command to stop the script from closing the terminal
+read -p "Press any key to exit..."
